cf5c68331872750896f2c60d8220b915ae0218c3
ebruaeo <ebruaeo@hotmail.com>
2024-08-23 13:44:38 +0300

    adding @GET annotation for movie details

--- app/src/main/java/com/example/moviesappusingretrofit/api/ApiService.kt
+++ app/src/main/java/com/example/moviesappusingretrofit/api/ApiService.kt
@@ -1,12 +1,18 @@
 package com.example.moviesappusingretrofit.api
 
+import com.example.moviesappusingretrofit.response.DetailsMovieResponse
 import retrofit2.Call
 import com.example.moviesappusingretrofit.response.MovieListResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
     @GET("movie/popular")
     fun getPopularMovie(@Query("page") page : Int) : Call<MovieListResponse>
+
+    @GET("movie/ {movie_id}")
+    fun getMovieDetails(@Path("movie_id") id: Int) : Call<DetailsMovieResponse>
+
 
 }
