bf39e6a351ee478504f282c1b82dffda4af92ab9
ebruaeo <ebruaeo@hotmail.com>
2024-08-23 12:33:44 +0300

    Constants setted

---
+++ app/src/main/java/com/example/moviesappusingretrofit/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.moviesappusingretrofit.utils
+
+object Constants {
+
+    const val API_KEY = "502abe68b4fc512bce4cb8358cde9d7b"
+    const val API_TOKEN =
+        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MDJhYmU2OGI0ZmM1MTJiY2U0Y2I4MzU4Y2RlOWQ3YiIsIm5iZiI6MTcyNDMyMjAzNy44ODI0MzUsInN1YiI6IjY2YzQ0YTUxZDU5ZTNhY2UzOGM5YmE2ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zgxmyq5hter2bNya7KMufikMz1PjAgR27l3JsO1-mvk"
+    const val BASEURL = "https://api.themoviedb.org/3/"
+    const val POSTER_BASEURL = "https://image.tmdb.org/t/p/w342"
+}
